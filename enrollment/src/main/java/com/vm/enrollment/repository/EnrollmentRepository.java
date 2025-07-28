504de01ac8c58432afcbb8a2fb2aa59a75ff50c5
sharan.r <sharan.r@valtech.com>
2025-07-28 18:03:34 +0530

    resolved permission issue

--- enrollment/src/main/java/com/vm/enrollment/repository/EnrollmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vm.enrollment.repository;
-
-import com.vm.enrollment.entity.Enrollment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
-}
