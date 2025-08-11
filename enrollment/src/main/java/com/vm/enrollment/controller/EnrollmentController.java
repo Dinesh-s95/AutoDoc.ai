419026924024ebd74d25929216b7a94b767ecef5
Dinesh S <dinesh.s@valtech.com>
2025-08-11 09:49:54 +0530

    test commit

--- enrollment/src/main/java/com/vm/enrollment/controller/EnrollmentController.java
+++ enrollment/src/main/java/com/vm/enrollment/controller/EnrollmentController.java
@@ -15,4 +15,9 @@ public class EnrollmentController {
     public String nominatePrimary(@RequestParam String username) {
         return service.nominatePrimaryUser(username);
     }
+
+    @PostMapping("/nominate-secondary")
+    public String nominateSecondaryUser(@RequestParam String username) {
+        return service.nominateSecondaryUser(username);
+    }
 }
